7bb56a01874ca786fdc21fbd871f137073c3a5cf
swethina <35322929+swethina@users.noreply.github.com>
2020-06-19 13:52:24 +0100

    Update base.java

--- Trello/src/test/java/pages/base.java
+++ Trello/src/test/java/pages/base.java
@@ -28,24 +28,14 @@ public class base {
 	try{
 	
 	DesiredCapabilities caps = new DesiredCapabilities();
-	//caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Le Max2");	//
 	caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3a_XL_API_27");
-
 	caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
-	//caps.setCapability(MobileCapabilityType.UDID, "1ece6047");
 	caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-
-	//caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1");
 	caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1");
 	caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 120);
-	//caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.android.chrome");
-	//caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "org.chromium.chrome.browser.ChromeTabbedActivity");
-	caps.setCapability("–session-override",true);
-	//caps.setCapability("appPackage","com.android.calculator2");
+	caps.setCapability("â€“session-override",true);
 	caps.setCapability("appPackage","com.trello");
 	caps.setCapability("appActivity", "com.trello.home.HomeActivity");
-	//caps.setCapability("appActivity","com.trello.feature.authentication.WelcomeActivity - Trello");
-	//caps.setCapability("appActivity","com.android.caliculator2.Calculator");
 	caps.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 	URL url = new URL("http://0.0.0.0:4723/wd/hub");
 	driver =  new AndroidDriver<MobileElement>(url,caps);
